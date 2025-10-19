24e5905d7f640a17477f8cf0cb9c7e339341b4da
bsnikhil-dev <nikhilbs1697@gmail.com>
2025-10-19 13:20:36 +0530

    Dynamic Form Elements added

--- client/src/components/Form/DynamicForm.tsx
+++ client/src/components/Form/DynamicForm.tsx
@@ -1,15 +1,25 @@
 import type { DynamicFormProps } from "../../types";
 import RenderFormFields from "../../utils/utilityComponents/RenderFormFields";
 
-const DynamicForm = ({ formControls, buttonText }: DynamicFormProps) => {
+const DynamicForm = ({
+  formControls,
+  formData,
+  onChange,
+  buttonText,
+  onSubmit,
+}: DynamicFormProps) => {
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <div>
         {formControls.map((formItem) => {
           return (
             <div key={formItem.name}>
               <label htmlFor={formItem.label}>{formItem.label}</label>
-              <RenderFormFields formItem={formItem} />
+              <RenderFormFields
+                formItem={formItem}
+                value={formData[formItem.name] || ""}
+                onChange={onChange}
+              />
             </div>
           );
         })}
--- client/src/utils/utilityComponents/RenderFormFields.tsx
+++ client/src/utils/utilityComponents/RenderFormFields.tsx
@@ -1,13 +1,80 @@
 import type { RenderFormFieldsProps } from "../../types";
 
 const RenderFormFields = ({
-  formItem: { type, name, placeholder },
+  formItem: { type, name, placeholder, options },
+  value,
+  onChange,
 }: RenderFormFieldsProps) => {
   switch (type) {
     case "text":
     case "email":
     case "password":
-      return <input type={type} placeholder={placeholder} id={name} />;
+      return (
+        <input
+          id={name}
+          name={name}
+          type={type}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
+      );
+    case "textarea":
+      return (
+        <textarea
+          id={name}
+          name={name}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
+      );
+    case "select":
+      return (
+        <select name={name} value={value} onChange={onChange}>
+          <option value="">Select</option>
+          {options?.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      );
+    case "radio":
+      return (
+        <div>
+          {options?.map((opt) => (
+            <label key={opt.value}>
+              <input
+                type="radio"
+                name={name}
+                value={opt.value}
+                checked={value === opt.value}
+                onChange={onChange}
+              />
+              {opt.label}
+            </label>
+          ))}
+        </div>
+      );
+    case "checkbox":
+      return (
+        <input
+          type="checkbox"
+          name={name}
+          checked={value}
+          onChange={(e) =>
+            onChange({
+              ...e,
+              target: {
+                ...e.target,
+                value: e.target.checked,
+                name: name,
+              },
+            })
+          }
+        />
+      );
   }
 };
 
